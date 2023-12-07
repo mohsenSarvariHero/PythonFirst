e1da445c98c928eda788a87e30095e09fa7ea199
mohsenSarvariHero <91257860+mohsenSarvariHero@users.noreply.github.com>
2023-12-07 23:36:59 +0330

    commit first program in python

---
+++ 1.py
@@ -0,0 +1,19 @@
+print("hello")
+print("mohsen")
+print ("sarvari")
+
+
+print("hello" , end='')
+print("  mohsen", end = " ")
+print ("  sarvari")
+
+# use ; to sepearate code in one line
+print("hello" , end= '') ; print(" mohsen" , end = " ") ;  print ("sarvari")
+# x = 4 + 4 + 41 + 2 + 1 or
+x = 4 \
+    + 4 \
+    + 41\
+    + \
+    2 + \
+     1
+print (x)
