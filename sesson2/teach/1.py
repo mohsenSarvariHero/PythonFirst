b8aa07a7b74f3a0ac10763be277ecdf63fe483ee
mohsenSarvariHero <91257860+mohsenSarvariHero@users.noreply.github.com>
2023-12-26 22:27:50 +0330

    finish sesson 2

---
+++ sesson2/teach/1.py
@@ -0,0 +1,20 @@
+print("hello")
+print("mohsen")
+print ("sarvari")
+
+
+print("hello" , end='')
+print("  mohsen", end = " ")
+print ("  sarvari")
+
+# use ; to sepearate code in one line
+print("hello" , end= '') ; print(" mohsen" , end = " ") ;  print ("sarvari")
+# x = 4 + 4 + 41 + 2 + 1 or
+# use \ to seperate coce in one line
+x = 4 \
+    + 4 \
+    + 41\
+    + \
+    2 + \
+     1
+print (x)
